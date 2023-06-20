7c73b56cd3a5d31136cc02858bed9e822dbce08d
Michael Anstis <manstis@redhat.com>
2023-06-20 11:26:49 +0100

    KOGITO-9419: Include SWF definitons in the Catalog

--- packages/backend/src/plugins/swf.ts
+++ packages/backend/src/plugins/swf.ts
@@ -20,12 +20,8 @@ import { PluginEnvironment } from '../types';
 export default async function createPlugin(
   env: PluginEnvironment,
 ): Promise<Router> {
-  // Here is where you will add all of the required initialization code that
-  // your backend plugin needs to be able to start!
-
-  // The env contains a lot of goodies, but our router currently only
-  // needs a logger
   return await createRouter({
+    eventBroker: env.eventBroker,
     logger: env.logger,
   });
 }
--- plugins/swf-common/src/constants.ts
+++ plugins/swf-common/src/constants.ts
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export * from './types';
-export * from './constants';
+export const topic = 'kogito-swf-service-ready';
