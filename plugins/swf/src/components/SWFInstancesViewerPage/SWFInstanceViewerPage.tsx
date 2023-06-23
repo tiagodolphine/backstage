3b42f137d47483fd971434e2cb2806bac56bb73c
Michael Anstis <manstis@redhat.com>
2023-06-23 10:18:04 +0100

    Add temporary [SWF]Instances page and navigation.

---
+++ plugins/swf/src/components/SWFInstancesViewerPage/SWFInstanceViewerPage.tsx
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 The Backstage Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { useEffect, useState } from 'react';
+import {
+  Content,
+  ContentHeader,
+  Header,
+  HeaderLabel,
+  InfoCard,
+  Page,
+  SupportButton,
+} from '@backstage/core-components';
+import { Grid } from '@material-ui/core';
+import { swfApiRef } from '../../api';
+import { useApi } from '@backstage/core-plugin-api';
+
+export const SWFInstancesViewerPage = () => {
+  const swfApi = useApi(swfApiRef);
+  const [instances, setInstances] = useState<any | undefined>(undefined);
+
+  useEffect(() => {
+    swfApi.getInstances().then(value => {
+      // const vars: string = value.data.ProcessInstances[0].variables as string;
+      // value.data.ProcessInstances[0].variables = JSON.parse(vars);
+      setInstances(value);
+    });
+  }, [swfApi]);
+
+  return (
+    <Page themeId="tool">
+      <Header
+        title="Serverless Workflow"
+        subtitle="Where all your SWF needs come to life!"
+      >
+        <HeaderLabel label="Owner" value="Team X" />
+        <HeaderLabel label="Lifecycle" value="Alpha" />
+      </Header>
+      <Content>
+        <ContentHeader title="Serverless Workflow - Instances">
+          <SupportButton>Orchestrate things with stuff.</SupportButton>
+        </ContentHeader>
+        <Grid container spacing={3} direction="column">
+          <Grid item>
+            <InfoCard title="Instances">
+              {instances && (
+                <pre>${JSON.stringify(instances, undefined, 2)}</pre>
+              )}
+            </InfoCard>
+          </Grid>
+        </Grid>
+      </Content>
+    </Page>
+  );
+};
