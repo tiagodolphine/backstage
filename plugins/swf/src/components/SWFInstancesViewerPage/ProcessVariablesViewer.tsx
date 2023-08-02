6bbfc88a2df0f9b64aa50100c8bbf21b0e5adcf7
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2023-08-02 15:48:01 -0300

    Fix ProcessVariablesViewer on dark mode

--- plugins/swf/src/components/SWFInstancesViewerPage/ProcessVariablesViewer.tsx
+++ plugins/swf/src/components/SWFInstancesViewerPage/ProcessVariablesViewer.tsx
@@ -17,6 +17,7 @@ import React from 'react';
 import ReactJson from 'react-json-view';
 
 import { InfoCard } from '@backstage/core-components';
+import { useTheme } from '@material-ui/core';
 
 interface ProcessVariablesViewerProps {
   variables: Record<string, unknown> | undefined;
@@ -24,11 +25,20 @@ interface ProcessVariablesViewerProps {
 
 export const ProcessVariablesViewer = (props: ProcessVariablesViewerProps) => {
   const { variables } = props;
+  const theme = useTheme();
 
   return (
     <InfoCard title="Variables">
       {variables === undefined && <p>No instance selected</p>}
-      <div>{variables && <ReactJson src={variables} name={false} />}</div>
+      <div>
+        {variables && (
+          <ReactJson
+            src={variables}
+            name={false}
+            theme={theme.palette.type === 'dark' ? 'monokai' : 'rjv-default'}
+          />
+        )}
+      </div>
     </InfoCard>
   );
 };
